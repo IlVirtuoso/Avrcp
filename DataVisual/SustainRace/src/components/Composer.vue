1f3e15207acebbcb2f07a15d371594444b6889da
ImSans <matteo.ielacqua@edu.unito.it>
2022-07-16 19:04:22 +0200

    add new typescript apps, start modeling a map grap

---
+++ DataVisual/SustainRace/src/components/Composer.vue
@@ -0,0 +1,33 @@
+<template>
+<p>Loaded</p>
+<Map id="Map" v-bind:dataIn="data" parameter="Coal (% electricity)" />
+</template>
+
+
+<script lang="ts">
+import { defineComponent} from "vue"
+import * as d3 from "d3"
+import Map from "./Map.vue"
+import DataManager from "./Support/DataManager"
+
+var datamanager = new DataManager();
+
+
+
+export default defineComponent({
+    data: () => { return { data: Object }; },
+    mounted() {
+        this.data = d3.csv("/share-elec-produc-by-source.csv") as any;
+        console.log(this.data);
+        console.log(datamanager.a);
+    },
+    methods: {
+        Load: function () {
+            return this.data;
+        }
+    },
+    components: { Map }
+});
+
+
+</script>
---
+++ DataVisual/SustainRace/src/components/Support/DataManager.ts
@@ -0,0 +1,7 @@
+export default class DataManager{
+
+    a : number = 0;
+    constructor(){
+        
+    }
+}
